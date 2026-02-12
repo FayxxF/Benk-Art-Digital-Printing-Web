6a8360f52348d29205c24b6a5308a37ae17c519e
FayxxF <86724715+FayxxF@users.noreply.github.com>
2026-02-12 22:22:44 +0700

    add PageController

--- app/Http/Controllers/PageController.php
+++ app/Http/Controllers/PageController.php
@@ -2,9 +2,20 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Product;
 use Illuminate\Http\Request;
 
 class PageController extends Controller
 {
-    //
+    public function index()
+    {
+        // Get 6 latest products for homepage
+        $products = Product::latest()->take(6)->get();
+        return view('layouts.app', compact('products'));
+    }
+
+    public function profile()
+    {
+        return view('pages.profile');
+    }
 }
